85d43304c23a55435f9a8b78123b8f6f56edef1f
DDDuoDuo <68424504+DDDuoDuo@users.noreply.github.com>
2024-12-12 09:40:31 -0800

    Updated MaxArrayDeque.java
    Added the Max() and Max(Comparator<T> c) methods

--- MaxArrayDeque.java
+++ MaxArrayDeque.java
@@ -1,158 +1,34 @@
 import java.util.Comparator;
-import java.util.Iterator;
 
-public class MaxArrayDeque<T> implements Deque<T> {
-    private T[] arr;
-    protected int nextFirst;
-    protected int nextLast;
-    private int size;
-    final protected int RFACTOR = 2;
+public class MaxArrayDeque<T> extends CircularArrayDeque<T> {
+    private Comparator<T> comp;
 
-    public MaxArrayDeque() {
-        arr = (T[]) new Object[8];
-        nextFirst = 4;
-        nextLast = 5;
-        size = 0;
-    }
+    public MaxArrayDeque() {}
 
     public MaxArrayDeque(Comparator<T> c) {
-        arr = (T[]) new Object[8];
-        nextFirst = 4;
-        nextLast = 5;
-        size = 0;
-    }
-
-    public MaxArrayDeque(T[] a, int nF, int nL) {
-        arr = a;
-        nextFirst = nF;
-        nextLast = nL;
-        size = a.length;
-    }
-
-    public void addFirst(T item) {
-        arr[nextFirst] = item;
-        size++;
-        if (nextFirst == nextLast) resize();
-        else {
-            nextFirst = Math.floorMod(nextFirst - 1, arr.length);
-//            if (nextFirst < 0) nextFirst = arr.length - 1;
-        }
-    }
-
-    public void addLast(T item) {
-        arr[nextLast] = item;
-        size++;
-        if (nextLast == nextFirst) resize();
-        else {
-            nextLast++;
-            if (nextLast >= arr.length) nextLast = 0;
-        }
-    }
-
-    public T removeFirst() {
-        int index = nextFirst + 1;
-        if (index >= arr.length) index = 0;
-        T tmp = arr[index];
-        arr[index] = null;
-        nextFirst = index;
-        return tmp;
-    }
-
-    public T removeLast() {
-        int index = nextLast - 1;
-        if (index < 0) index = arr.length - 1;
-        T tmp = arr[index];
-        arr[index] = null;
-        nextLast = index;
-        return tmp;
-    }
-
-    private void resize() {
-        T[] newArr = (T[]) new Object[arr.length * RFACTOR];
-        int index = arr.length / 2;
-        newArr[index] = arr[nextFirst];
-        nextFirst++;
-        index++;
-        while (nextFirst != nextLast) {
-            if (nextFirst >= arr.length) {
-                nextFirst = 0;
-            }
-            newArr[index] = arr[nextFirst];
-            nextFirst++;
-            index++;
-        }
-        nextFirst = arr.length / 2 - 1;
-        nextLast = nextFirst + arr.length + 1;
-        arr = newArr;
-    }
-
-    public boolean isEmpty() {
-        return (size == 0);
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public T get(int index) {
-        if (index >= size) return null;
-        int target = (nextFirst + index + 1) % arr.length;
-        return arr[target];
-    }
-
-    public void printDeque() {
-        System.out.println(this);
+        comp = c;
     }
 
     public T max() {
-        return null;
-    }
-
-    public T max(Comparator<T> c) {
-        return null;
-    }
-
-    @Override
-    public String toString() {
-        String ret = "{";
-        for (int i = 0; i < size(); i++) {
-            ret += get(i) + ", ";
+        if (size() == 0) return null;
+        T ret = this.get(0);
+        for (int i = 1; i < size(); i++) {
+            if (comp.compare(ret, get(i)) > 0) ret = get(i);
         }
-        ret = ret.substring(0, ret.length() - 2);
-        ret += "}";
         return ret;
     }
 
-    @Override
-    public boolean equals(Object other) {
-        MaxArrayDeque Deque = (MaxArrayDeque)  other;
-        if (size != Deque.size()) return false;
-        for (int i = 0; i < size(); i++) {
-            if (!get(i).equals(Deque.get(i))) return false;
+    public T max(Comparator<T> c) {
+        if (size() == 0) return null;
+        T ret = this.get(0);
+        for (int i = 1; i < size(); i++) {
+            if (c.compare(get(i), ret) > 0) ret = get(i);
         }
-        return true;
-    }
-
-    public Iterator<T> iterator() {
-        return new ArrayDequeIterator();
-    }
-
-    public class ArrayDequeIterator implements Iterator<T> {
-        int pos;
-        public ArrayDequeIterator() {
-            pos = 0;
-        }
-
-        public boolean hasNext() {
-            return (pos < size());
-        }
-
-        public T next() {
-            return arr[(nextFirst + pos++ + 1) % arr.length];
-        }
+        return ret;
     }
 
     public static void main(String[] args) {
+        Comparator<Integer> ic = (o1 ,o2) -> (o1-o2);
         MaxArrayDeque<Integer> array1 = new MaxArrayDeque<Integer>();
 
         array1.addFirst(3);
@@ -178,5 +54,7 @@ public class MaxArrayDeque<T> implements Deque<T> {
         System.out.println(array1);
         for (int i : array1) System.out.print(i + " ");
         System.out.println();
+
+        System.out.println(array1.max(ic));
     }
 }
